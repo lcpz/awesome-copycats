ce7a0a42ec65047f69e487d390264d022cd606e7
luke bonham <dadasignificanulla@gmail.com>
2014-01-09 14:33:33 +0100

    steamburn 3.0

--- themes/steamburn/theme.lua
+++ themes/steamburn/theme.lua
@@ -1,6 +1,7 @@
+
 --[[
                                      
-     Steamburn Awesome WM config 2.0 
+     Steamburn Awesome WM config 3.0 
      github.com/copycat-killer       
                                      
 --]]
@@ -11,19 +12,19 @@ themes_dir                          = os.getenv("HOME") .. "/.config/awesome/the
 theme.wallpaper                     = themes_dir .. "/wall.png"
 
 theme.font                          = "Tamsyn 10.5"
-theme.fg_normal                     = "#cdcdcd"
-theme.fg_focus                      = "#d79d38"
+theme.fg_normal                     = "#e2ccb0"
+theme.fg_focus                      = "#d88166"
 theme.fg_urgent                     = "#CC9393"
 theme.bg_normal                     = "#140c0b"
 theme.bg_focus                      = "#140c0b"
 theme.bg_urgent                     = "#2a1f1e"
-theme.border_width                  = "1"
-theme.border_normal                 = "#140c0f"
-theme.border_focus                  = "#915543"
+theme.border_width                  = 1
+theme.border_normal                 = "#302627"
+theme.border_focus                  = "#c2745b"
 theme.border_marked                 = "#CC9393"
-theme.taglist_fg_focus              = "#d47456"
+theme.taglist_fg_focus              = "#d88166"
 theme.tasklist_bg_focus             = "#140c0b"
-theme.tasklist_fg_focus             = "#d47456"
+theme.tasklist_fg_focus             = "#d88166"
 theme.menu_height                   = "16"
 theme.menu_width                    = "140"
 
@@ -48,5 +49,12 @@ theme.tasklist_disable_icon         = true
 theme.tasklist_floating             = ""
 theme.tasklist_maximized_horizontal = ""
 theme.tasklist_maximized_vertical   = ""
+
+-- lain related
+theme.layout_txt_termfair           = "[termfair]"
+theme.layout_txt_uselessfair        = "[ufv]"
+theme.layout_txt_uselessfairh       = "[ufh]"
+theme.layout_txt_uselessdwindle     = "[ud]"
+theme.layout_txt_uselesstile        = "[ut]"
 
 return theme
