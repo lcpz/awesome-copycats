79f7396b734340d7159b71399c67bc34acc4c719
Alexandre Viau <reazem@blueberry.Cisco>
2014-05-10 20:06:54 -0400

    Moved rules away from rc.lua.theme files

---
+++ settings/rules.lua
@@ -0,0 +1,37 @@
+-- {{{ Rules
+
+local awful = require("awful")
+local beautiful = require("beautiful")
+
+awful.rules.rules = {
+    -- All clients will match this rule.
+    { rule = { },
+      properties = { border_width = beautiful.border_width,
+                     border_color = beautiful.border_normal,
+                     focus = awful.client.focus.filter,
+                     keys = clientkeys,
+                     buttons = clientbuttons,
+                     size_hints_honor = false } },
+    { rule = { class = "URxvt" },
+          properties = { opacity = 0.99 } },
+
+    { rule = { class = "MPlayer" },
+          properties = { floating = true } },
+
+    { rule = { class = "Dwb" },
+          properties = { tag = tags[1][1] } },
+
+    { rule = { class = "Iron" },
+          properties = { tag = tags[1][1] } },
+
+    { rule = { instance = "plugin-container" },
+          properties = { tag = tags[1][1] } },
+
+    { rule = { class = "Gimp" },
+           properties = { tag = tags[1][4] } },
+
+    { rule = { class = "Gimp", role = "gimp-image-window" },
+          properties = { maximized_horizontal = true,
+                         maximized_vertical = true } },
+}
+-- }}}
