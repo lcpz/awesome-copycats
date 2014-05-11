00c993dcf36c703fe18ff40e10654bc7f564a281
Alexandre Viau <alexandre@alexandreviau.net>
2014-05-11 16:53:37 -0400

    Moved mouse bindings out of rc.lua.theme files

---
+++ settings/binds.lua
@@ -0,0 +1,9 @@
+local awful = require("awful")
+
+-- {{{ Mouse bindings
+root.buttons(awful.util.table.join(
+    awful.button({ }, 3, function () mymainmenu:toggle() end),
+    awful.button({ }, 4, awful.tag.viewnext),
+    awful.button({ }, 5, awful.tag.viewprev)
+))
+-- }}}
