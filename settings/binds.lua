7061b480e49a21b923aaf16e4a6159e1d77266b4
Alexandre Viau <alexandre@alexandreviau.net>
2014-05-11 18:02:36 -0400

    Moved layout manipulation, standard program and dropdown terminal binds to settings/binds.lua

--- settings/binds.lua
+++ settings/binds.lua
@@ -1,4 +1,5 @@
 local awful = require("awful")
+local drop = require("scratchdrop")
 
 -- {{{ Mouse bindings
 root.buttons(awful.util.table.join(
@@ -7,6 +8,42 @@ root.buttons(awful.util.table.join(
     awful.button({ }, 5, awful.tag.viewprev)
 ))
 -- }}}
+
+-- {{{ Key bindings
+
+globalkeys = awful.util.table.join(globalkeys,
+
+-- Layout manipulation
+awful.key({ modkey, "Shift"   }, "j", function () awful.client.swap.byidx(  1)    end),
+awful.key({ modkey, "Shift"   }, "k", function () awful.client.swap.byidx( -1)    end),
+awful.key({ modkey, "Control" }, "j", function () awful.screen.focus_relative( 1) end),
+awful.key({ modkey, "Control" }, "k", function () awful.screen.focus_relative(-1) end),
+awful.key({ modkey,           }, "u", awful.client.urgent.jumpto),
+awful.key({ modkey,           }, "Tab",
+    function ()
+        awful.client.focus.history.previous()
+        if client.focus then
+            client.focus:raise()
+        end
+    end),
+awful.key({ altkey, "Shift"   }, "l",      function () awful.tag.incmwfact( 0.05)     end),
+awful.key({ altkey, "Shift"   }, "h",      function () awful.tag.incmwfact(-0.05)     end),
+awful.key({ modkey, "Shift"   }, "l",      function () awful.tag.incnmaster(-1)       end),
+awful.key({ modkey, "Shift"   }, "h",      function () awful.tag.incnmaster( 1)       end),
+awful.key({ modkey, "Control" }, "l",      function () awful.tag.incncol(-1)          end),
+awful.key({ modkey, "Control" }, "h",      function () awful.tag.incncol( 1)          end),
+awful.key({ modkey,           }, "space",  function () awful.layout.inc(layouts,  1)  end),
+awful.key({ modkey, "Shift"   }, "space",  function () awful.layout.inc(layouts, -1)  end),
+awful.key({ modkey, "Control" }, "n",      awful.client.restore),
+
+-- Standard program
+awful.key({ modkey,           }, "Return", function () awful.util.spawn(terminal) end),
+awful.key({ modkey, "Control" }, "r",      awesome.restart),
+awful.key({ modkey, "Shift"   }, "q",      awesome.quit),
+
+-- Dropdown terminal
+awful.key({ modkey,	          }, "z",      function () drop(terminal) end)
+)
 
 -- Bind all key numbers to tags.
 -- Be careful: we use keycodes to make it works on any keyboard layout.
@@ -44,3 +81,5 @@ for i = 1, 9 do
                       end
                   end))
 end
+
+-- }}}
