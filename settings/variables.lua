34e4fd15903c67d46dd09d5072d3caf0713bac4d
Alexandre Viau <alexandre@alexandreviau.net>
2014-05-11 20:34:01 -0400

    Moved variables to ./settings/variables.lua

---
+++ settings/variables.lua
@@ -0,0 +1,12 @@
+-- common
+modkey     = "Mod4"
+altkey     = "Mod1"
+terminal   = "urxvtc" or "xterm"
+editor     = os.getenv("EDITOR") or "nano" or "vi"
+editor_cmd = terminal .. " -e " .. editor
+
+-- user defined
+browser    = "dwb"
+browser2   = "iron"
+gui_editor = "gvim"
+graphics   = "gimp"
