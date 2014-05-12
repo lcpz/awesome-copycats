fe44ee12a1cb30e0abce5898addc04cd165106e4
Alexandre Viau <reazem@blueberry.Cisco>
2014-05-11 22:54:41 -0400

    Moved Wallpaper to ./stock/wallpaper

---
+++ stock/wallpaper.lua
@@ -0,0 +1,10 @@
+local beautiful = require("beautiful")
+local gears = require("gears")
+
+-- {{{ Wallpaper
+if beautiful.wallpaper then
+    for s = 1, screen.count() do
+        gears.wallpaper.maximized(beautiful.wallpaper, s, true)
+    end
+end
+-- }}}
