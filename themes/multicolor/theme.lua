48c1e213975eb7b9cd78f17947c3012210299b5d
Eric Blanchette <eric.blanchette@digitalmatrices.com>
2015-05-11 21:33:52 -0400

    Updates

--- themes/multicolor/theme.lua
+++ themes/multicolor/theme.lua
@@ -11,7 +11,7 @@ theme                               = {}
 theme.confdir                       = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
 theme.wallpaper                     = theme.confdir .. "/wall.png"
 
-theme.font                          = "Terminus 8"
+theme.font                          = "Roboto 8"
 --theme.taglist_font                =
 theme.menu_bg_normal                = "#000000"
 theme.menu_bg_focus                 = "#000000"
