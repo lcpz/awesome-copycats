6241416ca57d554c9226ade1f41ad9445300c5ff
copycat-killer <dada@archlinux.info>
2017-01-29 16:36:09 +0100

    dremora modular; #closes 145

--- themes/dremora/theme.lua
+++ themes/dremora/theme.lua
@@ -1,17 +1,21 @@
 
 --[[
-                                   
-     Dremora Awesome WM config 2.0 
-     github.com/copycat-killer     
-                                   
+                                  
+     Dremora Awesome WM theme 2.0 
+     github.com/copycat-killer    
+                                  
 --]]
 
-local theme                                     = {}
+local gears   = require("gears")
+local lain    = require("lain")
+local awful   = require("awful")
+local wibox   = require("wibox")
+local os      = { getenv = os.getenv }
 
+local theme                                     = {}
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/dremora"
 theme.wallpaper                                 = theme.dir .. "/wall.png"
-
-theme.font                                      = "Tamsyn 10.5"
+theme.font                                      = "Misc Tamsyn 10.5"
 theme.taglist_font                              = "Icons 10"
 theme.fg_normal                                 = "#747474"
 theme.fg_focus                                  = "#DDDCFF"
@@ -25,15 +29,12 @@ theme.border_focus                              = "#292929"
 theme.titlebar_bg_focus                         = "#292929"
 theme.taglist_fg_focus                          = "#dddcff"
 theme.taglist_bg_focus                          = "#121212"
-
 theme.menu_height                               = 16
 theme.menu_width                                = 130
 theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
-
 theme.awesome_icon                              = theme.dir .."/icons/awesome.png"
 theme.taglist_squares_sel                       = theme.dir .. "/icons/square_sel.png"
 theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
-
 theme.layout_tile                               = theme.dir .. "/icons/tile.png"
 theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
 theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
@@ -46,12 +47,9 @@ theme.layout_max                                = theme.dir .. "/icons/max.png"
 theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
 theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
 theme.layout_floating                           = theme.dir .. "/icons/floating.png"
-
 theme.tasklist_plain_task_name                  = true
 theme.tasklist_disable_icon                     = true
-
 theme.useless_gap                               = 10
-
 theme.titlebar_close_button_focus               = theme.dir .. "/icons/titlebar/close_focus.png"
 theme.titlebar_close_button_normal              = theme.dir .. "/icons/titlebar/close_normal.png"
 theme.titlebar_ontop_button_focus_active        = theme.dir .. "/icons/titlebar/ontop_focus_active.png"
@@ -70,5 +68,181 @@ theme.titlebar_maximized_button_focus_active    = theme.dir .. "/icons/titlebar/
 theme.titlebar_maximized_button_normal_active   = theme.dir .. "/icons/titlebar/maximized_normal_active.png"
 theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/maximized_focus_inactive.png"
 theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
+
+awful.util.tagnames   = { "ƀ", "Ƅ", "Ɗ", "ƈ", "ƙ" }
+
+local markup     = lain.util.markup
+local separators = lain.util.separators
+local white      = theme.fg_focus
+local gray       = "#858585"
+
+-- Textclock
+local mytextclock = wibox.widget.textclock(markup(gray, " %a")
+.. markup(white, " %d ") .. markup(gray, "%b ") ..  markup(white, "%H:%M "))
+mytextclock.font = theme.font
+
+-- Calendar
+lain.widgets.calendar({
+    attach_to = { mytextclock },
+    notification_preset = {
+        font = "Misc Tamsyn 11",
+        fg   = white,
+        bg   = theme.bg_normal
+}})
+
+--[[ Mail IMAP check
+-- commented because it needs to be set before use
+local mail = lain.widgets.imap({
+    timeout  = 180,
+    server   = "server",
+    mail     = "mail",
+    password = "keyring get mail",
+    notification_preset = { fg = white }
+    settings = function()
+        mail  = ""
+        count = ""
+
+        if mailcount > 0 then
+            mail = "Mail "
+            count = mailcount .. " "
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, mail) .. markup(white, count)))
+    end
+})
+--]]
+
+-- MPD
+theme.mpd = lain.widgets.mpd({
+    settings = function()
+        mpd_notification_preset.fg = white
+        artist = mpd_now.artist .. " "
+        title  = mpd_now.title  .. " "
+
+        if mpd_now.state == "pause" then
+            artist = "mpd "
+            title  = "paused "
+        elseif mpd_now.state == "stop" then
+            artist = ""
+            title  = ""
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, artist) .. markup(white, title)))
+    end
+})
+
+-- /home fs
+theme.fs = lain.widgets.fs({
+    partition = "/home",
+    options = "--exclude-type=tmpfs",
+    notification_preset = { fg = white, bg = theme.bg_normal, font = "Misc Tamsyn 10.5" },
+    settings  = function()
+        fs_header = ""
+        fs_p      = ""
+
+        if tonumber(fs_now.used) >= 90 then
+            fs_header = " Hdd "
+            fs_p      = fs_now.used
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, fs_header) .. markup(white, fs_p)))
+    end
+})
+
+-- Battery
+local bat = lain.widgets.bat({
+    settings = function()
+        bat_header = " Bat "
+        bat_p      = bat_now.perc .. " "
+        widget:set_markup(markup.font(theme.font, markup(gray, bat_header) .. markup(white, bat_p)))
+    end
+})
+
+-- ALSA volume
+theme.volume = lain.widgets.alsa({
+    --togglechannel = "IEC958,3",
+    settings = function()
+        header = " Vol "
+        vlevel  = volume_now.level
+
+        if volume_now.status == "off" then
+            vlevel = vlevel .. "M "
+        else
+            vlevel = vlevel .. " "
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, header) .. markup(white, vlevel)))
+    end
+})
+
+-- Weather
+theme.weather = lain.widgets.weather({
+    city_id = 2643743, -- placeholder (London)
+    notification_preset = { fg = white }
+})
+
+-- Separators
+local first     = wibox.widget.textbox('<span font="Misc Tamsyn 4"> </span>')
+local arrl_pre  = separators.arrow_right("alpha", "#1A1A1A")
+local arrl_post = separators.arrow_right("#1A1A1A", "alpha")
+function theme.at_screen_connect(s)
+    -- Quake application
+    s.quake = lain.util.quake({ app = awful.util.terminal })
+
+    -- If wallpaper is a function, call it with the screen
+    if type(wallpaper) == "function" then
+        theme.wallpaper = theme.wallpaper(s)
+    end
+    gears.wallpaper.maximized(theme.wallpaper, s, true)
+
+    -- Tags
+    awful.tag(awful.util.tagnames, s, awful.layout.layouts)
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(awful.util.table.join(
+                           awful.button({ }, 1, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 3, function () awful.layout.inc(-1) end),
+                           awful.button({ }, 4, function () awful.layout.inc( 1) end),
+                           awful.button({ }, 5, function () awful.layout.inc(-1) end)))
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, awful.util.tasklist_buttons)
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = 18, bg = theme.bg_normal, fg = theme.fg_normal })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            first,
+            s.mytaglist,
+            arrl_pre,
+            s.mylayoutbox,
+            arrl_post,
+            s.mypromptbox,
+            first,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            wibox.widget.systray(),
+            first,
+            theme.mpd.widget,
+            --mail.widget,
+            theme.fs.widget,
+            bat.widget,
+            theme.volume.widget,
+            mytextclock,
+        },
+    }
+end
 
 return theme
