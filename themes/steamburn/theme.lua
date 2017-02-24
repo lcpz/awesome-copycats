fb4d8cb5e7aa7c7fd6cf3294b05a5f4c55fb4225
Luke Bonham <dada@archlinux.info>
2017-02-24 13:11:18 +0100

    steamburn: remove duplicate wibox code; #161

--- themes/steamburn/theme.lua
+++ themes/steamburn/theme.lua
@@ -235,9 +235,6 @@ function theme.at_screen_connect(s)
     s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, awful.util.tasklist_buttons)
 
     -- Create the wibox
-    s.mywibox = awful.wibar({ position = "top", screen = s, height = 18, bg = theme.bg_normal, fg = theme.fg_normal })
-
-    -- Create the wibox
     s.mywibox = awful.wibar({ position = "top", screen = s, height = 18 })
 
     -- Add widgets to the wibox
