ff4caa01b7f89fbc7fbc196c6c5215876847b09f
Luca CPZ <luca.cpz@gmail.com>
2018-09-14 10:53:39 +0200

    fix font names; closes #237

--- themes/vertex/theme.lua
+++ themes/vertex/theme.lua
@@ -248,12 +248,12 @@ local mywifisig = awful.widget.watch(
     function(widget, stdout)
         local carrier, perc = stdout:match("(%d)-(%d+)")
         local tiptext = stdout:gsub("(%d)-(%d+)", ""):gsub("%s+$", "")
+        perc = tonumber(perc)
 
-        if carrier == "1" then
+        if carrier == "1" or not perc then
             wificon:set_image(theme.wifidisc)
             wifitooltip:set_markup("No carrier")
         else
-            perc = tonumber(perc)
             if perc <= 5 then
                 wificon:set_image(theme.wifinone)
             elseif perc <= 25 then
